9dfc1ab7897c892b00d8ce09d9941dbd4bdb0ce2
radoslavboychev <radoslavboychev@gmail.com>
2021-02-15 17:05:25 +0100

    put the indexperformance print into the printinfo method

--- brainsJava/src/computer/Computer.java
+++ brainsJava/src/computer/Computer.java
@@ -5,22 +5,27 @@ public class Computer {
 	String processor;
 	double clockSpeed;
 	int memorySize;
-	
-	void setClockSpeed(String processorName,double clockSpeed)
-	{
+
+	void setClockSpeed(String processorName, double clockSpeed) {
 		this.processor = processorName;
 		this.clockSpeed = clockSpeed;
-		
+
 	}
-	void setMemorySize(int memorySize)
-	{
+
+	void setMemorySize(int memorySize) {
 		this.memorySize = memorySize;
 	}
-	void calculateIndexPerformance(double clockSpeed,int memorySize)
-	{
+
+	void calculateIndexPerformance(double clockSpeed, int memorySize) {
 		double indexPerformance;
-		indexPerformance = 10*(clockSpeed+memorySize);
+		indexPerformance = 10 * (clockSpeed + memorySize);
 		System.out.println("The index performance is: " + indexPerformance);
 	}
-	
+
+	void printInfo() {
+		System.out.println("+++++\nProcessor: " + processor + "\nProcessing speed: " + clockSpeed + " \nMemory size: "
+				+ memorySize);
+		calculateIndexPerformance(clockSpeed, memorySize);
+		System.out.println("-----");
+	}
 }
--- brainsJava/src/computer/Main.java
+++ brainsJava/src/computer/Main.java
@@ -13,23 +13,29 @@ public class Main {
 		
 		myComp1.setClockSpeed("AMD Ryzen 1300", 3.45);
 		myComp1.setMemorySize(1024);
-		myComp1.calculateIndexPerformance(myComp1.clockSpeed, myComp1.memorySize);
+		myComp1.printInfo();
+		//myComp1.calculateIndexPerformance(myComp1.clockSpeed, myComp1.memorySize);
+		
 		
 		myComp2.setClockSpeed("Intel Core i5 Q4K3", 2.42);
 		myComp2.setMemorySize(512);
-		myComp2.calculateIndexPerformance(myComp2.clockSpeed, myComp2.memorySize);
+		myComp2.printInfo();
+		//myComp2.calculateIndexPerformance(myComp2.clockSpeed, myComp2.memorySize);
 		
 		myComp3.setClockSpeed("Intel Core i3 S45P", 1.45);
 		myComp3.setMemorySize(2048);
-		myComp3.calculateIndexPerformance(myComp3.clockSpeed, myComp3.memorySize);
+		myComp3.printInfo();
+		//myComp3.calculateIndexPerformance(myComp3.clockSpeed, myComp3.memorySize);
 		
 		myComp4.setClockSpeed("AMD Ryzen 2412AB", 4.50);
 		myComp4.setMemorySize(8148);
-		myComp4.calculateIndexPerformance(myComp4.clockSpeed, myComp4.memorySize);
+		myComp4.printInfo();
+		//myComp4.calculateIndexPerformance(myComp4.clockSpeed, myComp4.memorySize);
 		
 		myComp5.setClockSpeed("AMD Pro Gamer Epic", 9.50);
 		myComp5.setMemorySize(10240);
-		myComp5.calculateIndexPerformance(myComp5.clockSpeed, myComp5.memorySize);
+		myComp5.printInfo();
+		//myComp5.calculateIndexPerformance(myComp5.clockSpeed, myComp5.memorySize);
 	}
 
 }
