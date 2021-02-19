24d8d09d747b3eb343f3d8299f0f1423421a5140
radoslavboychev <radoslavboychev@gmail.com>
2021-02-19 14:31:26 +0100

    Exercise with ArrayLists

---
+++ brainsJava/src/arrayListExercise/ArrayListProject.java
@@ -0,0 +1,23 @@
+package arrayListExercise;
+
+import java.util.ArrayList;
+
+public class ArrayListProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String[] fruit = new String[3];
+		fruit[0] = "Mango";
+		fruit[1] = "Apple";
+		fruit[2] = "Orange";
+		
+		ArrayList fruitList = new ArrayList();
+		fruitList.add("Mango");
+		fruitList.add("Apple");
+		fruitList.add("Orange");
+		System.out.println(fruitList);
+		
+	}
+
+}
