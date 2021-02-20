b3bbcb3d07bfa0b7ca665eb7fe8b22f40934bf0c
radoslavboychev <radoslavboychev@gmail.com>
2021-02-20 17:45:57 +0100

    OOD Homework v1 To-do - Make the sorting work

--- brainsJava/src/computer/Computer.java
+++ brainsJava/src/computer/Computer.java
@@ -1,28 +1,45 @@
 package computer;
 
-public class Computer {
+public class Computer implements Comparable<Computer> {
 
-	String processor;
-	double clockSpeed;
-	int memorySize;
-	double indexPerformance;
+	private String processor;
+	private double clockSpeed;
+	private int memorySize;
+	private double indexPerformance;
 
+	// sets the processor info (clock speed and name)
 	void setClockSpeed(String processorName, double clockSpeed) {
 		this.processor = processorName;
 		this.clockSpeed = clockSpeed;
-
 	}
 
+	// some default constructor, not sure if necessary :/
+	public Computer() {
+	}
+
+	// constructor for the computer, sets the processor, clock speed, memory size
+	// and
+	// calculates the performance index
+	public Computer(String processor, double clockSpeed, int memorySize) {
+		this.processor = processor;
+		this.clockSpeed = clockSpeed;
+		this.memorySize = memorySize;
+		calculateIndexPerformance(clockSpeed, memorySize);
+	}
+
+	// sets the memory size
 	void setMemorySize(int memorySize) {
 		this.memorySize = memorySize;
 	}
 
-	void calculateIndexPerformance(double clockSpeed, int memorySize) {
+	// calculate the index performance to be used in the constructor or by itself
+	public double calculateIndexPerformance(double clockSpeed, int memorySize) {
 
-		indexPerformance = 10 * (clockSpeed + memorySize);
+		return indexPerformance = 10 * (clockSpeed + memorySize);
 
 	}
 
+	// prints the info for he computer
 	void printInfo() {
 		System.out.println("+++++\nProcessor: " + processor + "\nProcessing speed: " + clockSpeed + " \nMemory size: "
 				+ memorySize);
@@ -30,4 +47,35 @@ public class Computer {
 		System.out.println("The index performance is: " + indexPerformance);
 		System.out.println("-----");
 	}
+
+	// getter for the processor name
+	public String getProcessorName() {
+		return this.processor;
+	}
+
+	// getter for clock speed
+	public double getClockSpeed() {
+		return this.clockSpeed;
+	}
+
+	// getter for the memory size
+	public int getMemorySize() {
+		return this.memorySize;
+	}
+
+	@Override
+	public int compareTo(Computer c) {
+		double ip = calculateIndexPerformance(clockSpeed, memorySize)
+				- c.calculateIndexPerformance(clockSpeed, memorySize);
+
+		if (ip > 0) {
+			return 1;
+		} else if (ip < 0) {
+
+			return -1;
+		} else
+			return 0;
+
+	}
+
 }
--- brainsJava/src/computer/Main.java
+++ brainsJava/src/computer/Main.java
@@ -4,33 +4,34 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Computer myComp1 = new Computer();
-		Computer myComp2 = new Computer();
+		Computer myComp1 = new Computer("Intel Core i3", 3.54, 5300);
+		Computer myComp2 = new Computer("AMD GamerXD 3521", 2.14, 16000);
+
+		// this uses the default constructor which sets everything to 0 or null
 		Computer myComp3 = new Computer();
-		Computer myComp4 = new Computer();
-		Computer myComp5 = new Computer();
 
-		myComp1.setClockSpeed("AMD Ryzen 1300", 3.45);
-		myComp1.setMemorySize(1024);
+		Computer myComp4 = new Computer("AMD Ryzen Pro 1300", 3.5, 2560);
+		Computer myComp5 = new Computer("Intel Core i5 KJRN", 5.4, 512);
+		Computer myComp6 = new Computer("AMD Ryzen 9 5900X", 3.7, 1028);
+		Computer myComp7 = new Computer("Intel Core i7 10700K", 5.6, 10506);
+
+//		myComp1.setClockSpeed("AMD Ryzen 1300", 3.45);
+//		myComp1.setMemorySize(1024);
+//		myComp1.getClockSpeed();
+//		myComp2.setClockSpeed("Intel Core i5 Q4K3", 2.42);
+//		myComp2.setMemorySize(512);
+
+		// now to print the info for all PCs
 		myComp1.printInfo();
-
-		myComp2.setClockSpeed("Intel Core i5 Q4K3", 2.42);
-		myComp2.setMemorySize(512);
 		myComp2.printInfo();
-
-		myComp3.setClockSpeed("Intel Core i3 S45P", 1.45);
-		myComp3.setMemorySize(2048);
 		myComp3.printInfo();
-
-		myComp4.setClockSpeed("AMD Ryzen 2412AB", 4.50);
-		myComp4.setMemorySize(8148);
 		myComp4.printInfo();
-
-		myComp5.setClockSpeed("AMD Pro Gamer Epic", 9.50);
-		myComp5.setMemorySize(10240);
 		myComp5.printInfo();
-
+		myComp6.printInfo();
+		myComp7.printInfo();
 		
+		System.out.println(myComp1.compareTo(myComp2));
+
 	}
 
 }
