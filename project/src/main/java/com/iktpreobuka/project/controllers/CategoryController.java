06d5c39f8c1a09ef8c47a88bf78c9bfe89162813
radoslavboychev <radoslavboychev@gmail.com>
2021-05-27 19:38:09 +0200

    Project - 3.0

--- project/src/main/java/com/iktpreobuka/project/controllers/CategoryController.java
+++ project/src/main/java/com/iktpreobuka/project/controllers/CategoryController.java
@@ -3,7 +3,11 @@ package com.iktpreobuka.project.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +16,7 @@ import com.iktpreobuka.project.entities.CategoryEntity;
 
 /**
  * 
- * Zadatak 2 - 2.2
+ * Zadatak 2 - 2.3
  *
  */
 
@@ -20,7 +24,7 @@ import com.iktpreobuka.project.entities.CategoryEntity;
 
 public class CategoryController {
 	@RequestMapping("/project/categories")
-	protected List<CategoryEntity> getDB() {
+	public List<CategoryEntity> getDB() {
 		List<CategoryEntity> categories = new ArrayList<>();
 
 		CategoryEntity c1 = new CategoryEntity(1, "music", "description 1");
@@ -36,7 +40,7 @@ public class CategoryController {
 
 	/**
 	 * 
-	 * Zadatak 2 - 2.3
+	 * Zadatak 2 - 2.4
 	 *
 	 */
 	@PostMapping("/project/categories")
@@ -46,8 +50,61 @@ public class CategoryController {
 				+ addedCategory.getCategoryDescription());
 		
 		System.out.println("Category added!\n");
+		
+		//add it to the db list
+		getDB().add(addedCategory);
 
 		return addedCategory;
+	}
+	
+	/**
+	 * Zadatak 2 - 2.5 
+	 */
+	@PutMapping("/project/categories/{id}")
+	public CategoryEntity editCategory(@PathVariable int id, @RequestBody CategoryEntity editedCategory)
+	{
+		for (CategoryEntity cat  : getDB()) {
+			if(cat.getId() == editedCategory.getId())
+			{
+				cat.setId(editedCategory.getId());
+				cat.setCategoyName(editedCategory.getCategoyName());
+				cat.setCategoryDescription(editedCategory.getCategoryDescription());
+				
+				return cat;
+			}
+		}
+		
+		return null;
+		
+	}
+	
+	/**
+	 * Zadatak 2 - 2.6
+	 */
+	@DeleteMapping("/project/categories/{id}")
+	public CategoryEntity deleteCategory(@PathVariable int id)
+	{
+		for (CategoryEntity cat : getDB()) {
+			if (cat.getId() == id) {
+				getDB().remove(id);
+				return cat;
+			}
+		}
+		return null;
+	}
+	/**
+	 * Zadatak 2 - 2.7
+	 */
+	@GetMapping("/project/categories/{id}")
+	public CategoryEntity getCategoryByID(@PathVariable int id)
+	{
+		for (CategoryEntity cat : getDB()) {
+			if(cat.getId() == id)
+			{
+				return cat;
+			}
+		}
+		return null;
 	}
 
 }
